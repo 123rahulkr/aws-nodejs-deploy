88336bb204e9a43612c63741d5288200a3dee8dc
123rahulkr <rkr201759@gmail.com>
2026-05-28 12:04:28 +0530

    get user details

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 app.get("/api/get", (req, res) => {
   res.send({ message: "hello from nodejs" });
 });
+app.get("/api/get_user_details", (req, res) => {
+  res.send({
+    user: {
+      name: "Rahul",
+      email: "rkrxx@gmail.com",
+      contact: 1234,
+    },
+  });
+});
 app.listen(8000, () => {
   console.log("listening at port 8000");
 });
